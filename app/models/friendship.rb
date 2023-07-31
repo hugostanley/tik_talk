d81a2e00f19f6439480d002534cb53f43ccba425
hugostanley <stanleyhugo06@gmail.com>
2023-07-31 20:23:25 +0800

    fix buton updates

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,8 +1,8 @@
 class Friendship < ApplicationRecord
-  belongs_to :recipient, class_name: 'User'
-  belongs_to :requestor, class_name: 'User'
+  belongs_to :recipient, class_name: "User"
+  belongs_to :requestor, class_name: "User"
   has_many :messages, dependent: :destroy
-  validates :status, inclusion: { in: %w[accepted rejected pending], message: '%<value>s is not a valid status' }
+  validates :status, inclusion: {in: %w[accepted rejected pending], message: "%<value>s is not a valid status"}
 
   after_create_commit do
     # OMGGGG
@@ -11,50 +11,39 @@ class Friendship < ApplicationRecord
     # then we subscribe to that user id, so per create_commit, we are subscribing to two users hence we are able
     # to update the screen of that user realtim BRO
     #
-    broadcast_replace_to requestor_id, partial: 'users/sent_request', locals: { request: self, user: recipient },
-                                       target: "user_#{recipient_id}"
-    broadcast_replace_to recipient_id, partial: 'users/received_request', locals: { request: self, user: requestor },
-                                       target: "user_#{requestor_id}"
+    broadcast_replace_to "user_#{recipient_id}_show", partial: "users/user", locals: {friendship: self, user: requestor},
+      target: "user_#{requestor_id}_profile"
 
-    broadcast_replace_to "user_#{requestor_id}_show", partial: 'users/user',
-                                                 locals: { friendship: self, user: recipient }, target: "user_#{recipient_id}"
+    broadcast_replace_to "user_#{requestor_id}_show", partial: "users/user", locals: {friendship: self, user: recipient},
+      target: "user_#{recipient_id}_profile"
 
-    broadcast_replace_to "user_#{recipient_id}_show", partial: 'users/user', locals: { friendship: self, user: requestor },
-                                                 target: "user_#{requestor_id}"
   end
 
   after_update_commit do
-    broadcast_replace_to requestor_id, partial: 'users/sent_request', locals: { request: self, user: recipient },
-                                       target: "user_#{recipient_id}"
-    broadcast_replace_to recipient_id, partial: 'users/received_request', locals: { request: self, user: requestor },
-                                       target: "user_#{requestor_id}"
+    broadcast_replace_to "user_#{recipient_id}_show", partial: "users/user", locals: {friendship: self, user: requestor},
+      target: "user_#{requestor_id}_profile"
 
-    broadcast_replace_to "user_#{requestor_id}_show", partial: 'users/user',
-                                                 locals: { friendship: self, user: recipient }, target: "user_#{recipient_id}"
-
-    broadcast_replace_to "user_#{recipient_id}_show", partial: 'users/user', locals: { friendship: self, user: requestor },
-                                                 target: "user_#{requestor_id}"
+    broadcast_replace_to "user_#{requestor_id}_show", partial: "users/user", locals: {friendship: self, user: recipient},
+      target: "user_#{recipient_id}_profile"
   end
 
   after_destroy_commit do
-    broadcast_replace_to 'friendship', partial: 'users/add_friend', locals: { user: recipient }, target: self
+    broadcast_replace_to "user_#{requestor_id}_show", partial: "users/user",
+      locals: {friendship: nil, user: recipient}, target: "user_#{recipient_id}_profile"
 
-    broadcast_replace_to "user_#{requestor_id}_show", partial: 'users/user',
-                                                 locals: { friendship: nil, user: recipient }, target: "user_#{recipient_id}"
-
-    broadcast_replace_to "user_#{recipient_id}_show", partial: 'users/user', locals: { friendship: nil, user: requestor },
-                                                 target: "user_#{requestor_id}"
+    broadcast_replace_to "user_#{recipient_id}_show", partial: "users/user", locals: {friendship: nil, user: requestor},
+      target: "user_#{requestor_id}_profile"
   end
 
   def self.find_friendship(id1, id2, status = nil)
     if status
       Friendship.find_by(recipient_id: id1, requestor_id: id2,
-                         status:) || Friendship.find_by(recipient_id: id2, requestor_id: id1,
-                                                        status:)
+        status:) || Friendship.find_by(recipient_id: id2, requestor_id: id1,
+          status:)
     else
       Friendship.find_by(recipient_id: id1,
-                         requestor_id: id2) || Friendship.find_by(recipient_id: id2,
-                                                                  requestor_id: id1)
+        requestor_id: id2) || Friendship.find_by(recipient_id: id2,
+          requestor_id: id1)
     end
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "debounce", to: "https://ga.jspm.io/npm:debounce@1.2.1/index.js"
+pin "mrujs", to: "https://ga.jspm.io/npm:mrujs@1.0.0/dist/index.module.js"
+pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.7.0/dist/morphdom-esm.js"
