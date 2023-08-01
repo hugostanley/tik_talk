37299b573e166ddd0da7e05244b557b73a29f913
hugostanley <stanleyhugo06@gmail.com>
2023-08-01 12:45:44 +0800

    chore: add default screens to tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
   ],
   theme: {
     screens: {
-      'xs': '320px'
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
     },
     extend: {
       fontFamily: {
