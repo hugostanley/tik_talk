8305222273b237e763f5ffaf6f7270d559b0a570
hugostanley <stanleyhugo06@gmail.com>
2023-08-02 20:15:34 +0800

    chore: standardrb lint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,38 +6,40 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :instantiate
 
-  def index; end
+  def index
+  end
 
   def search
     @results = User.search(params)
   end
 
   def add_friend
-    @friendship = Friendship.create(requestor_id: current_user.id, recipient_id: params[:id], status: 'pending')
+    @friendship = Friendship.create(requestor_id: current_user.id, recipient_id: params[:id], status: "pending")
   end
 
   def accept_request
-    friendship = Friendship.find_by(recipient_id: current_user.id, requestor_id: params[:id], status: 'pending')
-    friendship.update(status: 'accepted') if friendship
+    friendship = Friendship.find_by(recipient_id: current_user.id, requestor_id: params[:id], status: "pending")
+    friendship.update(status: "accepted") if friendship
   end
 
   def remove_friend
-    friendship = Friendship.find_friendship current_user.id, params[:id], 'accepted'
-
+    friendship = Friendship.find_friendship current_user.id, params[:id], "accepted"
     friendship&.destroy
+    # @friend = (friendship.requestor_id == current_user.id) ? friendship.recipient : friendship.requestor
+    # respond_to(&:turbo_stream)
   end
 
   def cancel_request
-    friendship = Friendship.find_by(requestor_id: current_user.id, recipient_id: params[:id], status: 'pending')
+    friendship = Friendship.find_by(requestor_id: current_user.id, recipient_id: params[:id], status: "pending")
     friendship&.destroy
   end
 
   def show
     @user = User.find_by_id(params[:id])
     if @user
-    @friendship = Friendship.find_friendship current_user.id, params[:id]
+      @friendship = Friendship.find_friendship current_user.id, params[:id]
     else
-      render 'users/_user_not_found'
+      render "users/_user_not_found"
     end
   end
 
